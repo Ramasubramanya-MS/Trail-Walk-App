57175912b451c8152a6ede65f0c05bbe37af7b4c
Ramasubramanya-MS <ramasubramanyams1729@gmail.com>
2025-04-06 21:20:25 +1000

    Add Routing and Controllers for Regions

--- NatureWalks/NatureWalks.API/Controllers/RegionsController.cs
+++ NatureWalks/NatureWalks.API/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NatureWalks.API.Data;
 using NatureWalks.API.Models.Domain;
+using NatureWalks.API.Models.DTOs;
 
 namespace NatureWalks.API.Controllers
 {
@@ -20,8 +21,24 @@ namespace NatureWalks.API.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = dbcontext.Regions.ToList();
-            return Ok(regions);
+            // Get Data from Database - Domain Models
+            var regionsDomainModel = dbcontext.Regions.ToList();
+
+            // Map Domain Models to DTOs
+            var regionDto = new List<RegionDTO>();
+            foreach (var regionDomain in regionsDomainModel)
+            {
+                regionDto.Add(new RegionDTO()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+            
+            // Return DTOs
+            return Ok(regionDto);
         }
 
         // Get Regions By Id
@@ -29,12 +46,120 @@ namespace NatureWalks.API.Controllers
         [Route("{Id:Guid}")]
         public IActionResult GetById(Guid Id)
         {
-            var region = dbcontext.Regions.Find(Id);
-            if(region == null)
+            // Get Region Domain Model from Database
+            var regionDomain = dbcontext.Regions.Find(Id);
+
+            if(regionDomain == null)
             {
                 return NotFound();
             }
-            return Ok(region);
+
+            // Map/Convert Region Domain Model to Region DTO
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+            
+            // Return DTO
+            return Ok(regionDto);
         }
+
+        // POST To Create New Region
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map or convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // Use Domain Model to create Region
+            dbcontext.Regions.Add(regionDomainModel);
+            dbcontext.SaveChanges();
+
+            // Map Domain Model back to DTO
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDomainModel);
+        }
+
+        // Update Region
+        // Put:
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            // Check if region exists
+            var regionDomainModel = dbcontext.Regions.FirstOrDefault(x =>x.Id == id);
+
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Map DTO to Domain model
+            regionDomainModel.Code = updateRegionRequestDto.Code;
+            regionDomainModel.Name = updateRegionRequestDto.Name;
+            regionDomainModel.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+            dbcontext.SaveChanges();
+
+            // Convert Domain Model to DTO
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+
+            };
+
+            return Ok(regionDto);
+        }
+
+
+        // Delete Region
+        // DELETE: 
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomainModel = dbcontext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Delete region
+            dbcontext.Regions.Remove(regionDomainModel);
+            dbcontext.SaveChanges();
+
+            // return deleted Region back
+            // map Domain Model to DTO
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+
+
     }
 }
--- NatureWalks/NatureWalks.API/obj/Debug/net9.0/NatureWalks.API.AssemblyInfo.cs
+++ NatureWalks/NatureWalks.API/obj/Debug/net9.0/NatureWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NatureWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+002aea1fa093147916174bdd97637ced738b44f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ac862c4aa3f536a17aa51e22bca102812915f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NatureWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NatureWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
